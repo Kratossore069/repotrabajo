f8403b19eabb1c010841066e9c28cc269dcc66d5
Kratossore069 <victorgabrielarostegui@gmail.com>
2022-05-17 09:33:02 +0100

    Empezando_testing_Mockito

--- SpringMongo/SpringMongo/src/test/java/com/bosonit/SpringMongo/SpringMongoApplicationTests.java
+++ SpringMongo/SpringMongo/src/test/java/com/bosonit/SpringMongo/SpringMongoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.bosonit.SpringMongo;
 
+import com.bosonit.SpringMongo.repositorio.PersonaRepositorio;
+import com.bosonit.SpringMongo.servicio.PersonaServicio;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class SpringMongoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private PersonaServicio personaServicio;
+
+	@MockBean
+	private PersonaRepositorio personaRepositorio;
+
+
 
 }
