1853bff2d1938fdedf5a1c4d92ae0a94945855e3
kratossore069 <victorgabrielarostegui@gmail.com>
2022-06-12 19:13:20 +0100

    Avances JWT / Faltan importaciones

--- ejercicio_final_Spring/src/main/java/com/bosonit/virtualtravel2/Virtualtravel2Application.java
+++ ejercicio_final_Spring/src/main/java/com/bosonit/virtualtravel2/Virtualtravel2Application.java
@@ -3,11 +3,30 @@ package com.bosonit.virtualtravel2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import com.bosonit.virtualtravel2.seguridad.JWTAuthorizationFilter;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Virtualtravel2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Virtualtravel2Application.class, args);
+	}
+
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/user").permitAll()
+				.anyRequest().authenticated();
+		}
 	}
 }
---
+++ ejercicio_final_Spring/src/main/java/com/bosonit/virtualtravel2/modelo/User.java
@@ -0,0 +1,22 @@
+package com.bosonit.virtualtravel2.modelo;
+
+public class User {
+    
+    private String user;
+    private String token;
+
+    public User(){}
+
+    public String getUser() {
+        return user;
+    }
+    public void setUser(String user) {
+        this.user = user;
+    }
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }   
+}
