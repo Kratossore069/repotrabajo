0859955f0607f15e68e2314e4c77f19ba7698bc6
kratossore069 <victorgabrielarostegui@gmail.com>
2022-06-13 09:59:02 +0100

    No hay errores de dependencias

--- ejercicio_final_Spring/src/main/java/com/bosonit/virtualtravel2/Virtualtravel2Application.java
+++ ejercicio_final_Spring/src/main/java/com/bosonit/virtualtravel2/Virtualtravel2Application.java
@@ -4,29 +4,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.bosonit.virtualtravel2.seguridad.JWTAuthorizationFilter;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class Virtualtravel2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Virtualtravel2Application.class, args);
 	}
 
-	@EnableWebSecurity
-	@Configuration
-	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+}
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
 				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
 				.authorizeRequests()
 				.antMatchers(HttpMethod.POST, "/user").permitAll()
 				.anyRequest().authenticated();
-		}
 	}
 }
