055c1f29b5c5511c13f1cc3836d6ff7443597c5d
kratossore069 <victorgabrielarostegui@gmail.com>
2022-06-17 12:19:24 +0100

    Intento nuevo con jwt y virtualtraveler

---
+++ virtualJWT/src/main/java/com/bosonit/virtualtraveler/servicio/SeguridadServicio.java
@@ -0,0 +1,23 @@
+package com.bosonit.virtualtraveler.servicio;
+
+import com.bosonit.virtualtraveler.repositorio.RepoSeguridad;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SeguridadServicio {
+    
+    @Autowired
+    RepoSeguridad repoSeguridad;
+
+    /**
+     * FUNCION QUE VALIDA UN USUARIO Y CONTRASENIA
+     * @param username
+     * @param password
+     * @return true or false
+     */
+    public boolean validar(String username,String password){
+        return repoSeguridad.validar(username, password);
+    }
+}
