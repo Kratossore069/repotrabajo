89bce5cca74cb9bd280b9a1a00117c384d5bfa41
Kratossore069 <victorgabrielarostegui@gmail.com>
2022-07-04 11:45:49 +0100

    Problemas con H2

--- virtualtraveler/src/main/java/com/bosonit/virtualtraveler/controlador/ReservaController.java
+++ virtualtraveler/src/main/java/com/bosonit/virtualtraveler/controlador/ReservaController.java
@@ -1,10 +1,13 @@
 package com.bosonit.virtualtraveler.controlador;
 
+import com.bosonit.virtualtraveler.modelo.Reserva;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.bosonit.virtualtraveler.servicio.ReservaServicio;
+
+import java.util.List;
 
 @Controller
 public class ReservaController {
@@ -15,5 +18,25 @@ public class ReservaController {
     @GetMapping("/saludo")
     public String saludo(){
         return reservaServicio.saludo();
+    }
+
+    @PostMapping("/reservar")
+    public Reserva hacerReserva(@RequestBody Reserva reserva){
+        return reservaServicio.hacerReserva(reserva);
+    }
+
+    @DeleteMapping("/borrar")
+    public void eliminarReserva(@RequestBody Reserva reserva){
+        reservaServicio.eliminarReserva(reserva);
+    }
+
+    @PutMapping("/update")
+    public Reserva updateReserva(@RequestBody Reserva reserva){
+        return reservaServicio.updateReserva(reserva);
+    }
+
+    @GetMapping("/listar")
+    public List<Reserva> getAllReservas(){
+        return reservaServicio.getAllReservas();
     }
 }
--- virtualtraveler/src/main/java/com/bosonit/virtualtraveler/servicio/ReservaServicio.java
+++ virtualtraveler/src/main/java/com/bosonit/virtualtraveler/servicio/ReservaServicio.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class ReservaServicio {
     
     @Autowired
-    ReservaRepo reservaRepo;
+    private ReservaRepo reservaRepo;
 
     /**
      * FUNCION PARA VER SI TODO FUNCIONA
