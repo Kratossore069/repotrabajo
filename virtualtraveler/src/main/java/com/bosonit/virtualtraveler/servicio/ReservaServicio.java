f3d98fcb391225f6ab1c8362d624494735542d27
Kratossore069 <victorgabrielarostegui@gmail.com>
2022-07-04 09:40:20 +0100

    Servicio acabado, toca controlador

--- virtualtraveler/src/main/java/com/bosonit/virtualtraveler/servicio/ReservaServicio.java
+++ virtualtraveler/src/main/java/com/bosonit/virtualtraveler/servicio/ReservaServicio.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 import com.bosonit.virtualtraveler.modelo.Reserva;
 import com.bosonit.virtualtraveler.repositorio.ReservaRepo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ReservaServicio {
     
@@ -26,11 +29,38 @@ public class ReservaServicio {
      * @return la reserva creada
      */
     public Reserva hacerReserva(Reserva reserva){
-        Reserva nuevaReserva = new Reserva(reserva.getId(),reserva.getDia(),reserva.getMes());
-        return reservaRepo.save(nuevaReserva);
+        List<Reserva> list = reservaRepo.findById(reserva.getId()).stream().toList();
+        Reserva nuevaReserva = null;
+        if (list.size() > 0) {
+            nuevaReserva = reservaRepo.save(reserva);
+        }
+        return nuevaReserva;
     }
 
-    
+    /**
+     * METODO QUE ELIMINA UNA RESERVA DE LA BDD
+     * @param reserva para eliminar
+     */
+    public void eliminarReserva(Reserva reserva){
+        reservaRepo.delete(reserva);
+    }
 
+    /**
+     * FUNCION QUE ACTUALIZA UNA RESERVA
+     * @param reserva para actualizar
+     * @return reserva actualizada
+     */
+    public Reserva updateReserva(Reserva reserva){
+        return reservaRepo.save(reserva);
+    }
 
+    /**
+     * FUNCION QUE MUESTRA TODAS LAS RESERVAS
+     * @return todas las reservas
+     */
+    public List<Reserva> getAllReservas(){
+        List<Reserva> list = new ArrayList<>();
+        reservaRepo.findAll().forEach(reserva -> list.add(reserva));
+        return list;
+    }
 }
