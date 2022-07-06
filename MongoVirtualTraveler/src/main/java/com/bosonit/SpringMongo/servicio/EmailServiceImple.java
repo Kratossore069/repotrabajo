67057b3b4bf589723e4b3e8976c2d1d1d29ff656
kratossore069 <victorgabrielarostegui@gmail.com>
2022-07-06 13:10:08 +0100

    Correo implementado

--- MongoVirtualTraveler/src/main/java/com/bosonit/SpringMongo/servicio/EmailServiceImple.java
+++ MongoVirtualTraveler/src/main/java/com/bosonit/SpringMongo/servicio/EmailServiceImple.java
@@ -1,93 +1,32 @@
 package com.bosonit.SpringMongo.servicio;
 
-import com.bosonit.SpringMongo.modelo.EmailDetails;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResource;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import java.io.File;
 
-public class EmailServiceImple implements EmailService {
+public class EmailServiceImple{
 
     @Autowired
     private JavaMailSender javaMailSender;
 
-    @Value("${spring.mail.username}") private String sender;
+    /**
+     * METODO QUE ENVIA UN CORREO CON LOS DATOS DE LA RESERVA
+     * @param destinatario
+     * @param contexto
+     * @param cuerpo
+     */
+    public void sendEmail(String destinatario, String contexto,String cuerpo){
+        SimpleMailMessage message=new SimpleMailMessage();
 
-    // Method 1
-    // To send a simple email
-    public String sendSimpleMail(EmailDetails details)
-    {
+        message.setFrom("App Spring Boot");
+        message.setTo(destinatario);
+        message.setText(cuerpo);
+        message.setSubject(contexto);
 
-        // Try block to check for exceptions
-        try {
+        javaMailSender.send(message);
 
-            // Creating a simple mail message
-            SimpleMailMessage mailMessage
-                    = new SimpleMailMessage();
-
-            // Setting up necessary details
-            mailMessage.setFrom(sender);
-            mailMessage.setTo(details.getRecipient());
-            mailMessage.setText(details.getMsgBody());
-            mailMessage.setSubject(details.getSubject());
-
-            // Sending the mail
-            javaMailSender.send(mailMessage);
-            return "Mail Sent Successfully...";
-        }
-
-        // Catch block to handle the exceptions
-        catch (Exception e) {
-            return "Error while Sending Mail";
-        }
-    }
-
-    // Method 2
-    // To send an email with attachment
-    public String
-    sendMailWithAttachment(EmailDetails details)
-    {
-        // Creating a mime message
-        MimeMessage mimeMessage
-                = javaMailSender.createMimeMessage();
-        MimeMessageHelper mimeMessageHelper;
-
-        try {
-
-            // Setting multipart as true for attachments to
-            // be send
-            mimeMessageHelper
-                    = new MimeMessageHelper(mimeMessage, true);
-            mimeMessageHelper.setFrom(sender);
-            mimeMessageHelper.setTo(details.getRecipient());
-            mimeMessageHelper.setText(details.getMsgBody());
-            mimeMessageHelper.setSubject(
-                    details.getSubject());
-
-            // Adding the attachment
-            FileSystemResource file
-                    = new FileSystemResource(
-                    new File(details.getAttachment()));
-
-            mimeMessageHelper.addAttachment(
-                    file.getFilename(), file);
-
-            // Sending the mail
-            javaMailSender.send(mimeMessage);
-            return "Mail sent Successfully";
-        }
-
-        // Catch block to handle MessagingException
-        catch (MessagingException e) {
-
-            // Display message when exception occurred
-            return "Error while sending mail!!!";
-        }
+        System.out.println("Correo de reserva enviado");
     }
 }
