480fb4b0c81e4b27d121a606584541bd267be12d
Kratossore069 <victorgabrielarostegui@gmail.com>
2022-10-14 09:04:50 +0100

    Prueba Octubre JWT

---
+++ jwtpruebaoctubre/src/main/java/com/example/jwtpruebaoctubre/JwtpruebaoctubreApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtpruebaoctubre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtpruebaoctubreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtpruebaoctubreApplication.class, args);
+	}
+
+}
---
+++ jwtpruebaoctubre/src/test/java/com/example/jwtpruebaoctubre/JwtpruebaoctubreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtpruebaoctubre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtpruebaoctubreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
