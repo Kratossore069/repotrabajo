d078701b98cbd3ac1a06db512e90284dd330a0c8
Kratossore069 <victorgabrielarostegui@gmail.com>
2022-10-17 14:07:30 +0100

    Proyecto eCommerce/ver detalle producto

--- eCommerce/src/main/java/com/portfolio/eCommerce/controlador/AdministradorController.java
+++ eCommerce/src/main/java/com/portfolio/eCommerce/controlador/AdministradorController.java
@@ -1,15 +1,26 @@
 package com.portfolio.eCommerce.controlador;
 
+import com.portfolio.eCommerce.modelo.Producto;
+import com.portfolio.eCommerce.servicio.ProductoServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
 
+    @Autowired
+    private ProductoServicio productoServicio;
+
     @GetMapping
-    public String home(){
+    public String home(Model model) {
+        List<Producto> productos = productoServicio.findAll();
+        model.addAttribute("productos", productos);
         return "administrador/home";
     }
 }
